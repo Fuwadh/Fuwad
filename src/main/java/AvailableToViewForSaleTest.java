047b0ac8f424b091ec8010b90811769cc88ba3d5
Fuwad <tipu086@gmail.com>
2021-09-08 17:25:25 +0100

    AvailableToViewForSaleTest add

--- src/main/java/AvailableToViewForSaleTest.java
+++ src/main/java/AvailableToViewForSaleTest.java
@@ -15,45 +15,57 @@ public class AvailableToViewForSaleTest {
 
         // Locators
         By acceptAll =  By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]");
-      //  By countyRegionDropDown = By.xpath("//*[@id=\"sel_region_dropdown\"]");
-       // By searchRegion  = By.xpath("//*[@id=\"sel_region_dropdown\"]/div/div/div[1]/input");
-       // By cork = By.xpath("//*[@id=\"sel_region_dropdown\"]/div/div/div[2]/div[1]");
-        //By areasDropdown = By.xpath("//*[@id=\"sel_locality_dropdown\"]");
-        //By searchAreas = By.xpath("//*[@id=\"searchInput\"]");
-        //By searchLocalities = By.xpath("//*[@id=\"sel_locality_dropdown\"]/div[1]/div/div[2]/div[1]/div[2]/div[1]/app-mh-check-box/div/div[2]");
-        //By Done = By.xpath("//*[@id=\"sel_locality_dropdown\"]/div[1]/div/div[1]/app-mh-button[2]/button/span");
-        //By priceDropdown = By.xpath("//*[@id=\"sel_max_price_dropdown\"]");
-        //By price = By.xpath("//*[@id=\"sel_max_price_dropdown\"]/div/div/div/div[14]");
-       // By type = By.xpath("//*[@id=\"sel_property_type_dropdown\"]");
-       // By searchProperty = By.xpath("//*[@id=\"sel_property_type_dropdown\"]/div/div/div[1]/div[2]/input");
-       // By allProperties = By.xpath("//*[@id=\"sel_property_type_dropdown\"]/div/div/div[2]/div[1]/app-mh-check-box/div/div[2]");
-       // By done2 = By.xpath("//*[@id=\"sel_property_type_dropdown\"]/div/div/div[1]/div[1]/app-mh-button[2]/button/span");
-       // By search = By.xpath("//*[@id=\"sel_search_button\"]/span");
-
+        By ViewingsDropDown = By.xpath("/html/body/app-root/div/header/app-header-container/app-desktop-header/div/nav/div[1]/div[3]/div[1]");
+        By AvailableToViewForSale  = By.xpath("/html/body/app-root/div/header/app-header-container/app-desktop-header/div/nav/div[1]/div[3]/div[2]/div[1]/a");
+        By CountyRegionDropdown = By.xpath("//*[@id=\"sel_region_dropdown\"]/span[2]");
+        By Search = By.xpath("//*[@id=\"sel_region_dropdown\"]/div/div/div[1]/input");
+        By DublinAll = By.xpath("//*[@id=\"sel_region_dropdown\"]/div/div/div[2]/div[1]");
+        By AreasDropdown = By.xpath("//*[@id=\"sel_locality_dropdown\"]");
+        By searchAreas = By.xpath("//*[@id=\"searchInput\"]");
+        By Dublin7 = By.xpath("//*[@id=\"sel_locality_dropdown\"]/div[1]/div/div[2]/div[2]/div[2]/div[8]/app-mh-check-box/div/div[2]");
+        By Done = By.xpath("//*[@id=\"sel_locality_dropdown\"]/div[1]/div/div[1]/app-mh-button[2]/button/span");
+        By priceDropdown = By.xpath("//*[@id=\"sel_max_price_dropdown\"]");
+        By price = By.xpath("//*[@id=\"sel_max_price_dropdown\"]/div/div/div/div[21]");
+        By type = By.xpath("//*[@id=\"sel_property_type_dropdown\"]");
+        By searchProperty = By.xpath("//*[@id=\"sel_property_type_dropdown\"]/div/div/div[1]/div[2]/input");
+        By allHouses = By.xpath("//*[@id=\"sel_property_type_dropdown\"]/div/div/div[2]/div/app-mh-check-box/div/div[2]");
+        By done2 = By.xpath("//*[@id=\"sel_property_type_dropdown\"]/div/div/div[1]/div[1]/app-mh-button[2]/button/span");
+        By StartDate = By.xpath("//*[@id=\"sel_start_date\"]");
+        By SelectStartDate = By.xpath("//*[@id=\"sel_start_date\"]/div/div/ngb-datepicker/div[2]/div/ngb-datepicker-month/div[3]/div[4]/div");
+        By EndDate = By.xpath("//*[@id=\"sel_end_date\"]");
+        By SelectEndDate = By.xpath("//*[@id=\"sel_end_date\"]/div/div/ngb-datepicker/div[2]/div/ngb-datepicker-month/div[5]/div[4]/div");
+        By Search2 = By.xpath("//*[@id=\"sel_search_button\"]/span");
 
         try {
             // launch chrome and go to url
             driver.get("https://myhome.ie");
             driver.findElement(acceptAll).click();
             Thread.sleep(2000);
-            //driver.findElement(countyRegionDropDown).click();
-           // driver.findElement(searchRegion).sendKeys("Cork");
-           // driver.findElement(cork).click();
-            //Thread.sleep(2000);
-           // driver.findElement(areasDropdown).click();
-          //  driver.findElement(searchAreas).sendKeys("Cork city");
-          //  driver.findElement(searchLocalities).click();
-          //  driver.findElement(Done).click();
-          //  Thread.sleep(2000);
-          //  driver.findElement(priceDropdown).click();
-          //  driver.findElement(price).click();
-          //  Thread.sleep(1000);
-          //  driver.findElement(type).click();
-          //  driver.findElement(searchProperty).sendKeys("All Properties");
-          //  driver.findElement(allProperties).click();
-          //  driver.findElement(done2).click();
-           // Thread.sleep(2000);
-           // driver.findElement(search).click();
+            driver.findElement(ViewingsDropDown).click();
+            driver.findElement(AvailableToViewForSale).click();
+            driver.findElement(CountyRegionDropdown).click();
+            driver.findElement(Search).sendKeys("Dublin");
+            driver.findElement(DublinAll).click();
+            Thread.sleep(5000);
+            driver.findElement(AreasDropdown).click();
+            driver.findElement(searchAreas).sendKeys("Dublin");
+            driver.findElement(Dublin7).click();
+            driver.findElement(Done).click();
+            Thread.sleep(2000);
+            driver.findElement(priceDropdown).click();
+            driver.findElement(price).click();
+            Thread.sleep(2000);
+            driver.findElement(type).click();
+            driver.findElement(searchProperty).sendKeys("All house");
+            driver.findElement(allHouses).click();
+            driver.findElement(done2).click();
+            Thread.sleep(2000);
+            driver.findElement(StartDate).click();
+            driver.findElement(SelectStartDate).click();
+            Thread.sleep(2000);
+            driver.findElement(EndDate).click();
+            driver.findElement(SelectEndDate).click();
+            driver.findElement(Search2).click();
 
 
 
